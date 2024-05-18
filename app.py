05b16cfaea7343d5196cde613974944ff41b5082
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2024-05-18 13:35:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -128,3 +128,7 @@ def wikilist():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
