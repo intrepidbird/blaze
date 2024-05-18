9209e9d3e4e748d75bdec4f9c3405a38c2e69256
Dhruv Rakhade <138519797+pointySphere@users.noreply.github.com>
2024-05-18 16:09:42 +0000

    Co-authored-by: FoxJavaGauss <FoxJavaGauss@users.noreply.github.com>

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
@@ -28,19 +29,19 @@ class User(db.Model, UserMixin):
 
 class Wiki(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128), unique = True, nullable = False)
+    name = db.Column(db.String(128), nullable = False)
     pages = db.relationship('Page', backref='wiki', lazy=True)
 
 class Page(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     content = db.Column(db.Text, nullable=False)
     wiki_id = db.Column(db.Integer, db.ForeignKey('wiki.id'), nullable=False) 
 
 db.init_app(app)
  
-with app.app_context():
-    db.create_all()
+app.app_context().push()
+db.create_all()
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired()])
@@ -94,18 +95,36 @@ def createwiki():
             os.makedirs(path)
         db.session.add(wiki)
         db.session.commit()
-        home_content = Page(content=form.home.data, name='home', wiki=wiki)
+        home_page_name = f'home_{wiki.id}'
+        home_content = Page(content=form.home.data, name=home_page_name, wiki=wiki)
         db.session.add(home_content)
         db.session.commit()
+        f = open(f"./wiki/{wiki_name}/home.md", "x")
+        f.write(form.home.data)
+        f.close()
         return redirect(url_for('index'))
     return render_template('createwiki.html', createwiki_form=form)
+
+
+def listize():
+    wikis = Wiki.query.all()
+    wiki_names = [wiki.name for wiki in wikis]
+    print(wiki_names)
 
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
-    
+
+@app.route("/wikilist")
+def wikilist():
+    wikis = Wiki.query.all()
+    wiki_names = [wiki.name for wiki in wikis]
+    return render_template("wikilist.html", wiki_names=wiki_names)
+
+@app.route("/wiki{}")
+
 @app.route("/")
 def index():
     return render_template("index.html")
